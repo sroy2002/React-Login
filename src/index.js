2d87f265b6e38bbed6c7d6020e542f11350f8f3f
Suranjana Roy <118478919+sroy2002@users.noreply.github.com>
2024-04-08 22:17:36 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,42 +2,58 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
-import Signup from './Components/Signup';
-import Profile from './Pages/Profile';
-import Questions from './Pages/Questions';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import FinalPage from './Pages/FinalPage';
-
-const router = createBrowserRouter([
-  {
-    path:"/",
-    element:<App />,
-  },
-  {
-    path:"/signup",
-    element:<Signup/>,
-  },
-  {
-    path:"/profile",
-    element:<Profile/>,
-  },
-  {
-    path:"/questions",
-    element:<Questions  />,
-  },
-  {
-    path:"/userProfile",
-    element: <FinalPage />,
-  }
-]);
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-   <RouterProvider router={router}/>
-  </React.StrictMode>
+  <HashRouter>
+    <App />
+  </HashRouter>
 );
+
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import './styles.css';
+// import Signup from './Components/Signup';
+// import Profile from './Pages/Profile';
+// import Questions from './Pages/Questions';
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
+// import FinalPage from './Pages/FinalPage';
+
+// const router = createBrowserRouter([
+//   {
+//     path:"/",
+//     element:<App />,
+//   },
+//   {
+//     path:"/signup",
+//     element:<Signup/>,
+//   },
+//   {
+//     path:"/profile",
+//     element:<Profile/>,
+//   },
+//   {
+//     path:"/questions",
+//     element:<Questions  />,
+//   },
+//   {
+//     path:"/userProfile",
+//     element: <FinalPage />,
+//   }
+// ]);
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//    <RouterProvider router={router}/>
+//   </React.StrictMode>
+// );
 
