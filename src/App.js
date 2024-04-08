e3e7a94c4af5f8308d50a1c667323d498bdef3ee
Suranjana Roy <118478919+sroy2002@users.noreply.github.com>
2024-04-08 22:20:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,14 +17,3 @@ function App() {
 }
 
 export default App;
-
-// import Signup from "./Components/Signup";
-// function App() {
-//   return (
-//     <div >
-//       <Signup />
-//     </div>
-//   );
-// }
-
-// export default App;
