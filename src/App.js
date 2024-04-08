6dfa459aeb38d4d11024ac6f4b936276c1b280da
Suranjana Roy <118478919+sroy2002@users.noreply.github.com>
2024-04-08 22:18:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import { Routes, Route } from "react-router-dom";
 import Signup from "./Components/Signup";
+import Profile from "./Pages/Profile";
+import Questions from "./Pages/Questions";
+import FinalPage from "./Pages/FinalPage";
 function App() {
   return (
-    <div >
-      <Signup />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/questions" element={<Questions />} />
+        <Route path="/userProfile" element={<FinalPage />} />
+      </Routes>
+    </>
   );
 }
 
 export default App;
+
+// import Signup from "./Components/Signup";
+// function App() {
+//   return (
+//     <div >
+//       <Signup />
+//     </div>
+//   );
+// }
+
+// export default App;
